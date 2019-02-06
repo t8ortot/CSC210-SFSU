44081b8c1ba3c903b74906031cfc600a1f7d7bc1
t8ortot <jimmyjclark@gmail.com>
2019-02-05 22:11:51 -0800

    Update Project 0

---
+++ Project 0/HelloWorldApp.java
@@ -0,0 +1,16 @@
+package helloworldapp;
+
+/**
+ *
+ * @author James Clark
+ */
+public class HelloWorldApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello James Clark!");
+    }
+    
+}
