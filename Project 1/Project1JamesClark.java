53322245802d61c0f29f39095d512ea5303ee3c8
t8ortot <jimmyjclark@gmail.com>
2019-02-06 23:18:47 -0800

    Update Project 1 variable names

--- Project 1/Project1JamesClark.java
+++ Project 1/Project1JamesClark.java
@@ -13,29 +13,29 @@ public class Project1JamesClark {
 
     /**
      * Compute the user's maximum heart rate
-     * @param Age User's age
-     * @param HR User's Heart Rate
-     * @param Max Formula for maximum heart rate
+     * @param age User's age
+     * @param heartRate User's Heart Rate
+     * @param max Formula for maximum heart rate
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter your age: ");
-        int Age = input.nextInt(); //User's input for age.
+        int age = input.nextInt(); //User's input for age.
 
         System.out.print("Please enter your current heart rate: ");
-        int HR = input.nextInt(); //User's input for heart rate.
+        int heartRate = input.nextInt(); //User's input for heart rate.
 
-        int Max = 220 - Age; //Calculating the user's maximum heart rate.
-        System.out.println("Your maximum heart rate is: " + Max);
+        int max = 220 - age; //Calculating the user's maximum heart rate.
+        System.out.println("Your maximum heart rate is: " + max);
 
-        if (HR == Max) { //Check if heart rate is equal to the maximum.
+        if (heartRate == max) { //Check if heart rate is equal to the maximum.
             System.out.println("Your heart rate is at the maximum!");
         
-        } else if (HR > Max) { //Check if heart rate is above the maximum.
+        } else if (heartRate > max) { //Check if heart rate is above the maximum.
             System.out.println("Your heart rate is above the maximum!");
         
-        } else if (HR < Max) { //Check if heart rate is below the maximum.
+        } else if (heartRate < max) { //Check if heart rate is below the maximum.
             System.out.println("Your heart rate is below the maximum.");
 
         }
