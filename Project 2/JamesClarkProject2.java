849db8055b3f5511633703fe68f2855e8e841f00
t8ortot <jimmyjclark@gmail.com>
2019-02-06 23:24:27 -0800

    Update Project 2 var names and README

--- Project 2/JamesClarkProject2.java
+++ Project 2/JamesClarkProject2.java
@@ -24,30 +24,30 @@ public class JamesClarkProject2 {
 
         System.out.print("Please enter your lower bound: ");
 
-        float Lower = input.nextInt(); //User input for lower bound
-        Lower /= 100; //Divides the lower bound by 100.
+        float lower = input.nextInt(); //User input for lower bound
+        lower /= 100; //Divides the lower bound by 100.
 
         System.out.print("Please enter your upper bound: ");
 
-        float Upper = input.nextInt(); //User input for upper bound
-        Upper /= 100; //Divides the upper bound by 100
+        float upper = input.nextInt(); //User input for upper bound
+        upper /= 100; //Divides the upper bound by 100
 
         System.out.println("Age\tTarget\tMaximum"); //generating columns
 
         int age = 20; //Setting list of ages at 20
-        int Max; //Maximum Heart rate displayed for each age
+        int max; //Maximum Heart rate displayed for each age
         float targetUp; //Value for Target Upper bound
         float targetLow; //Value for Target Lower bound
 
         while (age <= 70) { //While the age is below 70, repeat.
             System.out.print(age + "\t"); //Prints age
-            Max = 220 - age; //Calculates maximum HR
-            targetUp = Max * Upper; //Calculates Upper bound BPM
-            targetLow = Max * Lower; //Calculates Lower bound BPM
+            max = 220 - age; //Calculates maximum HR
+            targetUp = max * upper; //Calculates Upper bound BPM
+            targetLow = max * lower; //Calculates Lower bound BPM
             System.out.printf("%.0f", targetLow); //Prints lower bound with no decimals
             System.out.print("-");
             System.out.printf("%.0f", targetUp); //Prints upper bound with no decimals
-            System.out.println("\t" + Max);  //Prints Maximum BPM
+            System.out.println("\t" + max);  //Prints Maximum BPM
             age += 5; //Adds 5 to age
         }
 
--- Project 2/README.md
+++ Project 2/README.md
@@ -1,9 +1,9 @@
 # Project 2
 
-This program is meant to print a range of target and maximum heart rates, depending on age, as a table.
+This program is meant to print a range of target and maximum heart rates, depending on age, in a table.
 
 The table consists of 3 columns: Age (which is incremented by 5 each row), target range, and max.
 
-The maximum is calculated  as: (max = 220 - age)
-
 The target range is calculated as (lower = (lower bound %)(max)) and (upper = (upper bound %)(max))
+
+The maximum is calculated  as: (max = 220 - age)
